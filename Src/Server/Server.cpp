a347915fa23ae939c87493a68fd27969cd5cd4dd
zmakhkha <jcaladic@gmail.com>
2023-11-21 17:04:16 +0100

    chore : working on server logic + binding servers sockets + accepting incomming clinets using one poll + working on the structure of the requests.

--- Src/Server/Server.cpp
+++ Src/Server/Server.cpp
@@ -1,53 +1,96 @@
 
 
 #include "Server.hpp"
+#include <map>
 #include <netinet/in.h>
 #include <stdexcept>
 #include <sys/_types/_size_t.h>
 #include <sys/socket.h>
 #include <sys/poll.h>
 #include <arpa/inet.h>
+#include <vector>
 
 
 void MServer::run()
 {
 	unsigned int nserv = this->conf.getConfig().size();
 	sockaddr_in addrserv[nserv];
-	std::vector<int> servfd[nserv];
+	std::vector<int> servfd;
+	std::map<int, std::string> reqs;
 	for(size_t i = 0; i < nserv; i++)
 	{
-		servfd[i].push_back(socket(AF_INET, SOCK_STREAM, 0));
+		servfd.push_back(socket(AF_INET, SOCK_STREAM, 0));
 		addrserv[i].sin_family = AF_INET;
 		addrserv[i].sin_addr.s_addr = INADDR_ANY;
 		addrserv[i].sin_port = htons(this->conf.getConfig()[i].listen.second);
-		if(bind(servfd[i][0] , (struct sockaddr *)(&addrserv[i]), sizeof(addrserv[i])) == -1)
+		if(bind(servfd[i] , (struct sockaddr *)(&addrserv[i]), sizeof(addrserv[i])) == -1)
 			throw(std::runtime_error("Server: Socket failed to bind"));
-		if(listen(servfd[i][0], MAX_CLENTS) == -1)
+		if(listen(servfd[i], MAX_CLENTS) == -1)
 			throw(std::runtime_error("Server: Listening failed"));
 	}	
-	struct pollfd fds[nserv * (1 + MAX_CLENTS)];
-	bzero(fds, sizeof(fds));
-	for(size_t i = 0; i < nserv; i++)
-	{	
-		fds[i].fd = servfd[i][0];
-		fds[i].events = POLLIN;
+
+	std::vector<struct pollfd> fds;
+	for (size_t i = 0; i < nserv; i++)
+	{
+		struct pollfd a;
+		bzero(&a, sizeof(pollfd));
+		a.fd = servfd[i];
+		a.events = POLLIN;
+		fds.push_back(a);
 	}
 	while(true)
 	{
-		for(size_t i = 0; i < nserv; i++)
+		poll(fds.data(), fds.size(), -1);
+		fds.data();
+		for (size_t i = 0; i < nserv; i++)
 		{
 			if(fds[i].revents & POLLIN)
+			{	int client = accept(fds[i].fd, NULL, NULL);
+				if(client == -1)
+					std::cerr << "accept: A client refused to connect" << std::endl;
+				struct pollfd a;
+				bzero(&a, sizeof(pollfd));
+				a.fd = client;
+				a.events = POLLIN;
+				fds.push_back(a);
+				reqs[client] = "";
+			}
+		}
+		for(size_t j = nserv + 1; j < fds.size(); j++)
+		{
+			if(fds[j].revents & POLLIN)
 			{
-				int client = accept(fds[i].fd,NULL, NULL);
-				if(client != -1)
-				{
-					if
-				}
-				else
-					std::cerr << "accept: A client refuse to connect" << std::endl;
+				char *data = new char[PAGE];
+				int ret = recv(fds[j].fd, data, PAGE, 0);
+
+
 			}
 		}
 	}
+
+	// struct pollfd fds[nserv * (1 + MAX_CLENTS)];
+	// bzero(fds, sizeof(fds));
+	// for(size_t i = 0; i < nserv; i++)
+	// {	
+	// 	fds[i].fd = servfd[i][0];
+	// 	fds[i].events = POLLIN;
+	// }
+	// while(true)
+	// {
+	// 	for(size_t i = 0; i < nserv; i++)
+	// 	{
+	// 		if(fds[i].revents & POLLIN)
+	// 		{
+	// 			int client = accept(fds[i].fd,NULL, NULL);
+	// 			if(client != -1)
+	// 			{
+	// 				if
+	// 			}
+	// 			else
+	// 				std::cerr << "accept: A client refuse to connect" << std::endl;
+	// 		}
+	// 	}
+	// }
 
 
 	
--- Src/Server/Server.hpp
+++ Src/Server/Server.hpp
@@ -2,6 +2,8 @@
 
 #include "../ConfigFile/ConfigFile.hpp"
 #define MAX_CLENTS 10
+
+#define PAGE 4096
 class MServer
 {
 	private:
