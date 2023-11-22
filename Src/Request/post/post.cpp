b6cdd9961f065f517fb613cd8df516fc6d010523
zmakhkha <jcaladic@gmail.com>
2023-11-22 09:57:48 +0100

    Added form-data parsing + still to deal with files

--- Src/Request/post/post.cpp
+++ Src/Request/post/post.cpp
@@ -6,170 +6,146 @@
 /*   By: zmakhkha <zmakhkha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/16 12:49:02 by zmakhkha          #+#    #+#             */
-/*   Updated: 2023/11/21 20:46:30 by zmakhkha         ###   ########.fr       */
+/*   Updated: 2023/11/22 09:55:14 by zmakhkha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "post.hpp"
+#include <sys/_types/_size_t.h>
+#include <utility>
 
-post::post()
-{
-    _upPath = "/Users/zmakhkha/Desktop/wbsrv/upload/";
+post::post() { _upPath = "/Users/zmakhkha/Desktop/WebServer/upload/"; }
+
+post::~post() {}
+
+std::vector<st_> post::ft_split(const st_ req, const st_ del) {
+  std::vector<std::string> res;
+  size_t begin = 0;
+  size_t end = req.length();
+
+  while (begin < end) {
+    size_t pos = req.find(del, begin);
+
+    if (pos != std::string::npos) {
+      std::string t = req.substr(begin, pos - begin);
+      res.push_back(t);
+      begin = pos + del.length();
+    } else {
+      std::string t = req.substr(begin, end - begin);
+      res.push_back(t);
+      begin = end;
+    }
+  }
+  return res;
 }
 
-post::~post()
-{
+st_ post::fileToStr(const st_ path) {
+  std::ifstream file(path.c_str());
+  if (file.is_open()) {
+    st_ ctn;
+    char tmp;
+    while (file.get(tmp))
+      ctn += tmp;
+    file.close();
+    return ctn;
+  } else
+    throw std::runtime_error("Could Not Open File !!");
+  return NULL;
 }
 
-std::vector<st_> post::ft_split(const st_ req, const st_ del)
-{
-    std::vector<std::string> res;
-    size_t begin = 0;
-    size_t end = req.length();
+void post::detectBoundry() {
+  if (isValidReq()) {
 
-    while (begin < end)
-    {
-        size_t pos = req.find(del, begin);
+    Map mp = req.getVector();
+    st_ line = mp["Content-Type"];
+    this->_boundary = line.substr(line.find("boundary=") + 9, line.length());
+    this->_endBoundary = "--" + _boundary + "--";
+  }
+}
 
-        if (pos != std::string::npos)
-        {
-            std::string t = req.substr(begin, pos - begin);
-            res.push_back(t);
-            begin = pos + del.length();
+bool post::isValidReq() { return true; }
+
+void post::detectFields() {
+  st_ body = this->req.getBody();
+  _fields = ft_split(body, "\r\n--" + _boundary + "\r\n");
+}
+
+void post::detectBinaryFiles() {
+  detectFields();
+  for (size_t i = 0; i < _fields.size(); i++) {
+    if (_fields[i].find("filename") != st_::npos) {
+      _binFiles.push_back(_fields[i]);
+      _fields.erase(_fields.begin() + i);
+    }
+  }
+  size_t pos = _fields[_fields.size() - 1].find(_endBoundary);
+  if (_binFiles.size() && pos != st_::npos)
+    _binFiles[_binFiles.size() - 1] =
+        _binFiles[_binFiles.size() - 1].substr(0, pos);
+  // return (!_binFiles.size()) ? makeResponse(201) : parseFiles();
+}
+
+void post::makeResponse(int code) { std::cout << code << std::endl; }
+
+void post::parseFiles() {
+  for (size_t i = 0; i < _binFiles.size(); i++) {
+    size_t beg = _binFiles[i].find("filename");
+    if (beg != st_::npos) {
+      std::vector<st_> _spl = ft_split(_binFiles[i], "\r\n\r\n");
+      if (_spl.size()) {
+        st_ fileName = _spl[0].substr(beg + 10);
+        size_t end = fileName.find("\"\r\n");
+        fileName = fileName.substr(0, end);
+        _binFileNames.push_back(fileName);
+        st_ tmp = "";
+        for (size_t i = 1; i < _spl.size(); i++) {
+          tmp += _spl[i];
         }
-        else
-        {
-            std::string t = req.substr(begin, end - begin);
-            res.push_back(t);
-            begin = end;
-        }
+        _binFiles[i] = tmp;
+      }
     }
-    return res;
+  }
 }
 
-st_ post::fileToStr(const st_ path)
-{
-    std::ifstream file(path.c_str());
-    if (file.is_open())
-    {
-        st_ ctn;
-        char tmp;
-        while (file.get(tmp))
-            ctn += tmp;
-        file.close();
-        return ctn;
+void post::makeFiles() {
+  for (size_t i = 0; i < _binFiles.size(); i++) {
+    std::ofstream ofile(std::string(_upPath + _binFileNames[i].c_str()));
+
+    if (!ofile.is_open() || ofile.bad())
+      makeResponse(503);
+
+    ofile << _binFiles[i];
+    if (ofile.bad()) {
+      ofile.close();
+      makeResponse(503);
     }
-    else
-        throw std::runtime_error("Could Not Open File !!");
-    return NULL;
+    // ofile << std::endl;
+    ofile.close();
+  }
+  // makeResponse(201);
 }
 
-void post::detectBoundry()
-{
-    if (isValidReq())
-    {
-
-        Map mp = req.getVector();
-        st_ line = mp["Content-Type"];
-        this->_boundary = line.substr(line.find("boundary=") + 9, line.length());
-        this->_endBoundary = "--" + _boundary + "--";
-    }
-}
-
-bool post::isValidReq()
-{
-    return true;
-}
-
-void post::detectFields()
-{
-    st_ body = this->req.getBody();
-    _fields = ft_split(body, "\r\n--" + _boundary + "\r\n");
-}
-
-void post::detectBinaryFiles()
-{
+void post::runPost(void) {
+  if (isValidReq()) {
+    std::string st =
+        fileToStr("/Users/zmakhkha/Desktop/WebServer/Src/req_example");
+    this->req = request(st);
+    detectBoundry();
     detectFields();
-    for (size_t i = 0; i < _fields.size(); i++)
-    {
-        if (_fields[i].find("filename") != st_::npos)
-            _binFiles.push_back(_fields[i]);
-    }
-    size_t pos = _fields[_fields.size() - 1].find(_endBoundary);
-    if (_binFiles.size() && pos != st_::npos)
-        _binFiles[_binFiles.size() - 1] = _binFiles[_binFiles.size() - 1].substr(0, pos);
-    // return (!_binFiles.size()) ? makeResponse(201) : parseFiles();
+    detectBinaryFiles();
+    detectDataFields();
+    parseFiles();
+    makeFiles();
+
+  } else
+    makeResponse(403);
 }
 
-void post::makeResponse(int code)
-{
-    std::cout << code << std::endl;
-}
-
-void post::parseFiles()
-{
-    for (size_t i = 0; i < _binFiles.size(); i++)
-    {
-        size_t beg = _binFiles[i].find("filename");
-        if (beg != st_::npos)
-        {
-            std::vector<st_> _spl = ft_split(_binFiles[i], "\r\n\r\n");
-            if (_spl.size())
-            {
-                st_ fileName = _spl[0].substr(beg + 10);
-                size_t end = fileName.find("\"\r\n");
-                fileName = fileName.substr(0, end);
-                _binFileNames.push_back(fileName);
-                st_ tmp = "";
-                for (size_t i = 1; i < _spl.size(); i++)
-                {
-                    tmp += _spl[i];
-                }
-                _binFiles[i] = tmp;
-            }
-        }
-    }
-    // makeFiles();
-}
-
-void post::makeFiles()
-{
-    for (size_t i = 0; i < _binFiles.size(); i++)
-    {
-        std::ofstream ofile(std::string(_upPath + _binFileNames[i].c_str()));
-
-        if (!ofile.is_open() || ofile.bad())
-            makeResponse(503);
-
-        ofile << _binFiles[i];
-        if (ofile.bad())
-        {
-            ofile << std::endl;
-            ofile.close();
-            makeResponse(503);
-        }
-        ofile << std::endl;
-        ofile.close();
-    }
-    makeResponse(201);
-}
-
-void post::runPost(void)
-{
-    if (isValidReq() )
-    {
-        std::string st = fileToStr("/Users/zmakhkha/Desktop/wbsrv/Src/req_example");
-        this->req = request(st);
-        detectBoundry();
-        detectFields();
-        detectBinaryFiles();
-        std::cout << "------>" << _binFiles.size() << std::endl;
-        parseFiles();
-        makeFiles();
-        
-        
-    }
-    else
-        makeResponse(403);
-    
+void post::detectDataFields() {
+  for (size_t i = 0; i < _fields.size(); i++) {
+    st_ key = ft_split(_fields[i], "name=\"")[1];
+    key = key.substr(0, key.find('"'));
+    st_ val = _fields[i].substr(_fields[i].find_last_of("\r\n") + 1);
+    _dataFields.push_back(std::make_pair(key, val));
+  }
 }
--- Src/Request/post/post.hpp
+++ Src/Request/post/post.hpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <map>
+#include <utility>
 
 #include "../Request.hpp"
 class post
@@ -9,18 +10,20 @@ private:
     st_ _endBoundary;
     st_ _upPath;
     std::vector<st_> _fields;
+    std::vector<std::pair<st_, st_> > _dataFields;
     std::vector<st_> _binFiles;
     std::vector<st_> _binFileNames;
 
 public:
 request req;
-    post(/* args */);
+    post();
     ~post();
     std::vector<st_> ft_split(const st_ req, const st_ del);
     st_ fileToStr(const st_ path);
     void detectBoundry();
     bool isValidReq();
     void detectFields();
+    void detectDataFields();
     void detectBinaryFiles();
     void makeResponse(int code);
     void makeFiles();
