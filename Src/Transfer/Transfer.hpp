750b18f2ceb51cc0884bbca23aa4c79d97c1f9ab
zmakhkha <jcaladic@gmail.com>
2023-12-03 18:19:42 +0100

    chore : Post Req with Boundary done

--- Src/Transfer/Transfer.hpp
+++ Src/Transfer/Transfer.hpp
@@ -18,11 +18,14 @@ struct Transfer
     int contentlen;
     std::string chunk;
     std::string boundary;
+    std::string fileData;
     bool reading;
+    std::string page1;
+    std::string page2;
     Transfer()
     {
         contentlen = 0;
-        chunk = "";
+        boundary = "----------------------------683634263385122688561624";
         reading = 1;
         cgi = 0;
         fd = open("cgi", O_CREAT | O_RDWR, 0644);
--- tst.cpp
+++ tst.cpp
@@ -2,9 +2,11 @@
 #include <algorithm>
 #include <cstddef>
 #include <cstdio>
+#include <cstdlib>
 #include <iostream>
 #include <stack>
 #include <string>
+#include <sys/_types/_ssize_t.h>
 #include <unistd.h>
 #include <sys/fcntl.h>
 
@@ -44,23 +46,27 @@ void execboundary(std::string s, std::string boundary)
 
 }
 
-void parseboundary(std::string &chunk, Transfer &A)
+void parseboundary(std::string chunk, Transfer &A)
 {
+
 	std::string line = chunk.substr(0, chunk.find("\r\n"));
 	chunk.erase(0, line.length() + 2);
-	if(line == A.boundary + "--")
+	// std::cout << line << std::endl << A.boundary + "--" << std::endl;
+	if (line == A.boundary + "--")
+	{
+		A.reading = 0;
 		return ;
+	}
 	int pos = chunk.find("filename=\"");
 	if(pos != std::string::npos)
 	{	
-		std::string file = chunk.substr(pos + 10, (chunk.find("\r\n") - pos - 11)) + std::string("**");
-		std::cout << file << std::endl;
+		std::string file = chunk.substr(pos + 10, (chunk.find("\r\n") - pos - 11));
+		// std::cout << file << std::endl;
 		if(A.fd > 0)
 			close(A.fd);
-		A.fd = open(file.c_str(), O_CREAT | O_RDWR, 0644);
+		std::string filename = "/goinfre/zmakhkha/up/" + file;
+		A.fd = open(filename.c_str(), O_CREAT | O_RDWR, 0644);
 	}
-
-	
 
 }
 
@@ -78,14 +84,13 @@ void  parseheaders(std::string &page, Transfer &A)
 	if(pos == std::string::npos)
 		std::cout << "bad request" << std::endl;
 	std::string headers = page.substr(0 , pos + 2);
-	A.boundary = "----------------------------313942177698852897111436";
 	page.erase(page.begin(), page.begin() + pos + 4);
 }
 
 
 
 
-void parsepage(std::string page, int ret, Transfer &A)
+void parsepage(std::string &page, Transfer &A)
 {
 	static bool a;
 	if(!a)
@@ -98,10 +103,10 @@ void parsepage(std::string page, int ret, Transfer &A)
 			std::string line = page.substr(0, page.find("\r\n"));
 			A.contentlen = hextodec(line);
 			if(!A.contentlen)
-				return (A.reading = 0, (void)0);
+				return (A.reading = 0, exit(0));
 			page.erase(page.begin(), page.begin() + line.size() + 2);
 		}
-		if(A.contentlen < page.length())
+		if(A.contentlen < page.length() )
 		{
 			A.chunk += page.substr(0, A.contentlen);
 			page.erase(page.begin(), page.begin() + A.contentlen + 2);
@@ -114,8 +119,50 @@ void parsepage(std::string page, int ret, Transfer &A)
 		A.chunk += page;
 		A.contentlen -= page.length();
 	}
-
 	a = true;
+}
+
+bool validboundary(std::string tmp, Transfer &A)
+{
+	size_t pos = tmp.find(A.boundary);
+	if (pos == std::string::npos)
+		return false;
+
+	std::string chunk = tmp.substr(0, pos);
+	tmp.erase(0, pos);
+	
+	pos = tmp.find("\r\n\r\n");
+	if(pos == std::string::npos && tmp.substr(0, A.boundary.length() + 2) != A.boundary + "--")
+		return false;
+	write(A.fd, chunk.c_str(), chunk.length() - 2);
+
+	std::string bounds = tmp.substr(0, pos);
+	parseboundary(bounds, A);
+	// std::cout << bounds << std::endl;
+	// exit(0);
+	std::string chunk2 = tmp.substr(pos + 4, tmp.length());
+	A.page1 = chunk2;
+	pos = A.page1.find(A.boundary);
+	A.page2 = "";
+	return true;
+}
+
+void parse2pages(std::string &page, Transfer &A)
+{
+	static bool a;
+	if(!a)
+		parseheaders(page, A);
+	a = true;
+	if(A.page1.empty())
+		return (A.page1 = page, (void)0);
+	if(A.page2.empty())
+		A.page2 = page;
+	if(!validboundary(A.page1 + A.page2, A))
+	{
+		write(A.fd, A.page1.c_str(), A.page1.length());
+		std::swap(A.page1, A.page2);
+		A.page2 = "";
+	}
 }
 
 int main()
@@ -123,16 +170,20 @@ int main()
 
 
 
-	int fd = open("ex", O_RDWR);
+	int fd = open("/goinfre/zmakhkha/req_exp", O_RDWR);
 	Transfer A;
+	// parseboundary("----------------------------119293058853605376629782\r\n", A);
+	// exit(0);
 	while(A.reading)
 	{
-		char *chunk = new char[4096];
-		int ret = read(fd, chunk, 4096);
+		char *chunk = new char[65356];
+		int ret = read(fd, chunk, 65356);
 		std::string str(chunk, ret);
-		parsepage(str, ret, A);
+		// std::cout << str;
+		// exit(0);
+		// parsepage(str, A);
+		parse2pages(str, A);
 		delete [] chunk;
-	
 	}
 }
 
